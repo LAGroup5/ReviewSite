be7c8e50559bb31711c221ebbf861aeb34a22ec3
littl3birdy <ashlynmmorin@gmail.com>
2021-05-24 16:14:35 -0700

    created TrailsList component and TrialTile component for React front end

---
+++ src/main/frontend/client/components/TrailsList.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect } from "react"
+
+const TrailsList = props => {
+    const [trails, setTrails] = useState([])
+    const getTrails = async () => {
+        try{
+            const response = await fetch("/api/v1/trails")
+            if (!response.ok) {
+                const errMessage = `${response.status} (${response.statusText})`
+                const err = new Error(errMessage)
+                throw err
+            }
+            const responseBody = await response.json()
+            setTrails(responseBody)
+        } catch (err) {
+            console.error(`Could not complete data fetch.`)
+        }
+    }
+
+    useEffect(() => {
+        getTrails()
+    }, [props])
+
+    const trailObjects = trails.map(trail => {
+        return <TrailTile
+        key ={trail.id}
+        id = {trail.id}
+        trailName ={trail.name}
+        difficultyRating = {trail.difficultyRating}
+        familyFriendly = {trail.familyFriendly}
+        petFriendly={trail.petFriendly}
+        />
+    })
+
+    return (
+        <div >
+            {trailObjects}
+        </div>
+    )
+}
+
+export default TrailsList
