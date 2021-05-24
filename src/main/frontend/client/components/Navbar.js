5c246cdfe9e7a64f72b7e27d1ae77e3a72ae4289
Ami Drammeh <amidrammeh1@gmail.com>
2021-05-24 16:23:55 -0700

    models update/navbar update

---
+++ src/main/frontend/client/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Navbar = props => {
+  return (
+    <div>
+      <ul id="nav">
+        <li>
+          <a href="/trails">Home</a>
+        </li>
+        <li>
+          <a href="#">About the Dev</a>
+        </li>
+
+        <li>
+          <h1>Seattle Trails</h2>
+          </li>
+
+        </ul>
+
+        </div>
+        )
+      }
+
+        export default Navbar
