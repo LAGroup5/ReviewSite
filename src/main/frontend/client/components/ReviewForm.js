ba52021656b132f3593ccda437f45f8d713389df
Steve Taylor <malakesh@hotmail.com>
2021-05-26 14:34:09 -0700

    adding changes to display review form on trailShow page

--- src/main/frontend/client/components/ReviewForm.js
+++ src/main/frontend/client/components/ReviewForm.js
@@ -1,70 +1,76 @@
-import React, { useState, useEffect } from "react"
-import _ from "lodash"
+import React, { useState, useEffect } from "react";
+import _ from "lodash";
 
-const ReviewForm = props => {
+const ReviewForm = (props) => {
   const defaultFormValues = {
     reviewerName: "",
     starRating: "",
-    review: ""
+    review: "",
+  };
+  const [newReview, setNewReview] = useState(defaultFormValues);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [redirect, setRedirect] = useState(false);
 
-   }
-   const [newReview, setNewReview] = useState(defaultFormValues)
-   const [formSubmitted, setFormSubmitted] = useState(false)
+  const addNewReview = async () => {
+    try {
+      const id = props.id;
+      const response = await fetch(`/api/v1/trails/${id}/review`, {
+        method: "POST",
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+        body: JSON.stringify(newReview),
+      });
+      if (!response.ok) {
+        if (response.status === 422) {
+          const data = await response.json();
+          return setErrors(data.errors);
+        } else {
+          const errorMessage = `${response.status} (${response.statusText})`;
+          const error = new Error(errorMessage);
+          throw error;
+        }
+      } else {
+        const data = await response.json();
+        if (data) {
+          props.handleWhatToShow();
+        }
+      }
+    } catch (error) {
+      console.error(`Error in fetch: ${error}`);
+    }
+  };
 
-   const addNewReview = async () => {
-       try {
-         const response = await fetch("/api/v1/trails/{id}/review", {
-           method: "POST",
-           headers: new Headers({
-             "Content-Type": "application/json"
-           }),
-           body: JSON.stringify(newReview)
-         })
-         if (!response.ok) {
-           if (response.status === 422) {
-             const data = await response.json()
-             return setErrors(data.errors)
-           } else {
-             const errorMessage = `${response.status} (${response.statusText})`
-             const error = new Error(errorMessage)
-             throw error
-           }
-         } else {
-           const data = await response.json()
-           if (data) {
-             setRedirect(true)
-           }
-         }
-       } catch (error) {
-         console.error(`Error in fetch: ${error}`)
-       }
-     }
+  const handleChange = (event) => {
+    setNewReview({
+      ...newReview,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addNewReview(newReview);
+  };
 
-   const handleChange = event => {
-      setNewReview({
-        ...newReview,
-        [event.currentTarget.name]: event.currentTarget.value
-      })
-     }
+  const clearForm = () => {
+    setNewReview(defaultFormValues);
+  };
 
-   const handleSubmit = event => {
-       event.preventDefault()
-       addNewReview(newReview)
-     }
-
-     const clearForm = () => {
-       setNewReview(defaultFormValues)
-     }
-
-     if (!formSubmitted) {
-         return (
-           <form className="new-review" onSubmit={handleSubmit}>
-           <label htmlFor="name">
-           Your Name:
-          <input name="reviewerName" id="reviewerName" type="text" value={newReview.reviewerName} onChange={handleChange} />
-           </label>
-          <label htmlFor="rating">
+  if (!formSubmitted) {
+    return (
+      <form className="new-review" onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Your Name:
+          <input
+            name="reviewerName"
+            id="reviewerName"
+            type="text"
+            value={newReview.reviewerName}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="rating">
           Rating:
           <input
             type="text"
@@ -74,25 +80,26 @@ const ReviewForm = props => {
             onChange={handleChange}
           />
         </label>
+        <br />
 
         <label htmlFor="review">
           Review:
-        <textarea
+          <textarea
             name="review"
             id="review"
-            rows="5" cols="33"
+            rows="5"
+            cols="33"
             value={newReview.review}
             onChange={handleChange}
           />
         </label>
 
-
         <div>
           <input className="button" type="submit" value="Submit Review" />
         </div>
       </form>
-         )
-     }
-    }
+    );
+  }
+};
 
-  export default ReviewForm
+export default ReviewForm;
