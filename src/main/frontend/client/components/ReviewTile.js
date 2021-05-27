310f8d21933f83b409c35ab3be2b4f5aa96b99fa
Steve Taylor <malakesh@hotmail.com>
2021-05-27 11:37:59 -0700

    updated html elements to organize page and allow for future css styling work

--- src/main/frontend/client/components/ReviewTile.js
+++ src/main/frontend/client/components/ReviewTile.js
@@ -1,15 +1,17 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ReviewTile = (props) => {
   return (
     <div>
-      <p className="reviewer-name">{props.reviewerName}</p>
-      <p className="review-star-rating">{props.starRating}</p>
+      <p className="reviewer-name">
+        {props.reviewerName}
+        <span className="review-star-rating">{props.starRating} Stars</span>
+      </p>
       <p className="review-body">{props.body}</p>
-      <br />
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default ReviewTile
+export default ReviewTile;
--- src/main/frontend/client/components/TrailShow.js
+++ src/main/frontend/client/components/TrailShow.js
@@ -1,74 +1,74 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import ReviewTile from "./ReviewTile.js"
-import ReviewForm from "./ReviewForm.js"
-import SuccessTile from "./SuccessTile.js"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ReviewTile from "./ReviewTile.js";
+import ReviewForm from "./ReviewForm.js";
+import SuccessTile from "./SuccessTile.js";
 
 const TrailShow = (props) => {
-  const [trail, setTrail] = useState([])
-  const [reviews, setReviews] = useState([])
-  const [showReviewForm, setReviewShowForm] = useState(false)
-  const [afterSubmission, setAfterSubmission] = useState(false)
-  const [showResponse, setShowResponse] = useState(null)
-  const trailId = props.match.params.id
+  const [trail, setTrail] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [showReviewForm, setReviewShowForm] = useState(false);
+  const [afterSubmission, setAfterSubmission] = useState(false);
+  const [showResponse, setShowResponse] = useState(null);
+  const trailId = props.match.params.id;
 
   const getTrail = async () => {
     try {
-      const response = await fetch(`/api/v1/trails/${trailId}`)
+      const response = await fetch(`/api/v1/trails/${trailId}`);
       if (!response.ok) {
-        const errorMessage = `${response.status} (${response.statusText})`
-        const error = new Error(errorMessage)
-        throw error
+        const errorMessage = `${response.status} (${response.statusText})`;
+        const error = new Error(errorMessage);
+        throw error;
       }
-      const responseBody = await response.json()
-      setTrail(responseBody)
+      const responseBody = await response.json();
+      setTrail(responseBody);
     } catch (err) {
-      console.error(`Could not complete data fetch.`)
+      console.error(`Could not complete data fetch.`);
     }
-  }
+  };
 
   const getReviews = async () => {
-    try{
-      const response = await fetch(`/api/v1/trails/${trailId}/all-reviews`)
+    try {
+      const response = await fetch(`/api/v1/trails/${trailId}/all-reviews`);
       if (!response.ok) {
-        const errMessage = `${response.status} (${response.statusText})`
-        const err = new Error(errMessage)
-        throw err
+        const errMessage = `${response.status} (${response.statusText})`;
+        const err = new Error(errMessage);
+        throw err;
       }
-      const responseBody = await response.json()
-      setReviews(responseBody)
+      const responseBody = await response.json();
+      setReviews(responseBody);
     } catch (err) {
-      console.error(`Could not complete data fetch`)
+      console.error(`Could not complete data fetch`);
     }
-  }
+  };
 
   useEffect(() => {
-    getTrail()
-    getReviews()
-  }, [props])
+    getTrail();
+    getReviews();
+  }, [props]);
 
   const showNewReviewForm = (event) => {
-    event.preventDefault()
-    setReviewShowForm(true)
-  }
+    event.preventDefault();
+    setReviewShowForm(true);
+  };
 
   const handleWhatToShow = () => {
-    setAfterSubmission(true)
-  }
+    setAfterSubmission(true);
+  };
 
-  let reviewFormDisplay = ""
+  let reviewFormDisplay = "";
   if (showReviewForm) {
     reviewFormDisplay = (
       <ReviewForm
         id={props.match.params.id}
         handleWhatToShow={handleWhatToShow}
       />
-    )
+    );
   }
 
-  let whatToShow = ""
+  let whatToShow = "";
   if (afterSubmission) {
-    whatToShow = <SuccessTile />
+    whatToShow = <SuccessTile />;
   } else {
     whatToShow = (
       <div>
@@ -77,7 +77,7 @@ const TrailShow = (props) => {
         </button>
         {reviewFormDisplay}
       </div>
-    )
+    );
   }
 
   const ReviewObjects = reviews.map((review) => {
@@ -89,52 +89,82 @@ const TrailShow = (props) => {
         starRating={review.starRating}
         body={review.body}
       />
-    )
-  })
+    );
+  });
 
-  let petFriendly
+  let petFriendly;
   if (trail.petFriendly) {
-    petFriendly = "Yes"
+    petFriendly = "Yes";
   } else {
-    petFriendly = "No"
+    petFriendly = "No";
   }
 
-  let familyFriendly
+  let familyFriendly;
   if (trail.familyFriendly) {
-    familyFriendly = "Yes"
+    familyFriendly = "Yes";
   } else {
-    familyFriendly = "No"
+    familyFriendly = "No";
   }
 
-  let passesRequired
-  if (trail.passesRequired) {
-    passesRequired = "Pass Required For Entry"
+  let difficultyRating;
+  if (trail.difficulty === 1) {
+    difficultyRating = "Easy";
+  } else if (trail.difficulty === 2) {
+    difficultyRating = "Intermediate";
   } else {
-    passesRequired = "Free Entry, No Pass Required"
+    difficultyRating = "Advanced";
+  }
+
+  let passesRequired;
+  if (trail.passesRequired) {
+    passesRequired = "Pass Required For Entry";
+  } else {
+    passesRequired = "Free Entry, No Pass Required";
   }
 
   return (
     <div>
       <div className="trail-show">
         <h1>{trail.name}</h1>
-        <p>Difficulty Level: {trail.difficulty}</p>
-        <ul className="no-bullet trail-details">
-          <li>{trail.hikeLengthMiles} miles</li>
-          <li>{trail.elevationGainFt} ft</li>
-        </ul>
+        <span>{trail.description}</span>
+        <ul></ul>
+        <table>
+          <colgroup>
+            <col style={{ width: 150 }}></col>
+            <col style={{ width: 100 }}></col>
+            <col style={{ width: 150 }}></col>
+          </colgroup>
+          <thead>
+            <tr>
+              <th className="trail-details-head, trail-details-left">
+                Difficulty Level
+              </th>
+              <th className="trail-details-head">Distance</th>
+              <th className="trail-details-head">Elevation Gain</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="trail-body">
+              <th className="trail-details-left">{difficultyRating} </th>
+              <th className="trail-details">{trail.hikeLengthMiles} miles</th>
+              <th className="trail-details">{trail.elevationGainFt} ft</th>
+            </tr>
+          </tbody>
+        </table>
+
         <ul className="no-bullet">
           <li>Family Friendly: {familyFriendly}</li>
           <li>Pet Friendly: {petFriendly}</li>
           <li>{passesRequired}</li>
         </ul>
-      </div>
-      <div>{whatToShow}</div>
-      <div>
-        <h2>Reviews</h2>
-        {ReviewObjects}
+        <div>{whatToShow}</div>
+        <div>
+          <h2>Reviews</h2>
+          {ReviewObjects}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TrailShow
+export default TrailShow;
