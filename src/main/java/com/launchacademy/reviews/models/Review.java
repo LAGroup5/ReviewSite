f4fbb70eb201171bf772b74e2ae723c19fbd8117
Steve Taylor <malakesh@hotmail.com>
2021-05-28 10:21:42 -0700

    model validation for reviewRating, css update for form

--- src/main/java/com/launchacademy/reviews/models/Review.java
+++ src/main/java/com/launchacademy/reviews/models/Review.java
@@ -1,6 +1,7 @@
 package com.launchacademy.reviews.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Range;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,10 +28,15 @@ public class Review {
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reviews_generator")
   @Column(name = "id", nullable = false, unique = true)
   private Integer id;
+
   @Column(name = "reviewer_name")
   private String reviewerName;
+
+  @NotNull
+  @Range(min=1, max=5)
   @Column(name = "star_rating", nullable = false)
   private Integer starRating;
+
   @Column(name = "body")
   private String body;
 
